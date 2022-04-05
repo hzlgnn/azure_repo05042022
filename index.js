be411b6823ab2fef6c0b317e7762b33434df927a
hzlgnn <gonen.hzl@gmail.com>
2022-04-05 15:19:22 +0400

    api updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/api/showkey', async (req, res) => {
     })
 app.get('/api/merhaba', (req, res) => {
 
-    res.send('Merhaba Dünya')
+    res.send('Merhaba Dünyali')
 })
 
 app.post('/api/name', (req, res) => {
